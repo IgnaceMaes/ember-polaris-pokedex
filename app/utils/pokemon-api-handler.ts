07aa1a612f2196a97c5d0463aec9754c2bce435d
Chris Thoburn <runspired@users.noreply.github.com>
2024-09-12 09:02:11 +0100

    16 - DEMO2: optimize simple fetch case

--- app/utils/pokemon-api-handler.ts
+++ app/utils/pokemon-api-handler.ts
@@ -11,6 +11,37 @@ export const PokemonHandler: Handler = {
       });
     }
 
+    // in the findRecord case, we can check the cache first
+    // since our individual finds are always the same data as
+    // we get from list endpoints.
+    if (request.op === 'findRecord') {
+      const { store } = context.request;
+      const identifier = store.identifierCache.getOrCreateDocumentIdentifier(
+        context.request,
+      );
+      const peeked = identifier ? store.cache.peekRequest(identifier) : null;
+
+      // if we don't have a request yet, try peeking the identifier
+      if (!peeked) {
+        const identifier = request.records?.[0];
+        const record = identifier ? store.cache.peek(identifier) : null;
+
+        // if the cache has an entry, generate a fake response
+        if (record) {
+          const headers = new Headers();
+          headers.set('date', new Date().toUTCString());
+          const response = new Response(null, {
+            headers,
+          });
+          context.setResponse(response);
+
+          return {
+            data: record,
+          } as T;
+        }
+      }
+    }
+
     return next(request).then((response) => {
       fixLinkOrigin(response.content);
       return response;
