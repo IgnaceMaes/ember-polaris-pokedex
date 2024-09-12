3bc97e545fcaa4ccd7c77cfbf5a1faa4520efaee
Chris Thoburn <runspired@users.noreply.github.com>
2024-09-12 03:02:28 +0100

    7 - setup CachePolicy

--- app/services/store.ts
+++ app/services/store.ts
@@ -1,4 +1,5 @@
 import RequestManager from '@ember-data/request';
+import { CachePolicy } from '@ember-data/request-utils';
 import Fetch from '@ember-data/request/fetch';
 
 import BaseStore, { CacheHandler } from '@ember-data/store';
@@ -8,4 +9,9 @@ export default class Store extends BaseStore {
   requestManager = new RequestManager()
     .use([PokemonHandler, Fetch])
     .useCache(CacheHandler);
+
+  lifetimes = new CachePolicy({
+    apiCacheHardExpires: 1000 * 60 * 60 * 48, // 48 hours
+    apiCacheSoftExpires: 1000 * 60 * 60, // 1 hour
+  });
 }
